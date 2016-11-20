a80173da6341b7ba464b33386fdad836225f145a
dachengxu <xudacheng06@163.com>
2016-11-20 14:58:36 +0800

    a general function

--- helloword.c
+++ helloword.c
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int add(int* a, int len)
+{
+	int sum = 0;
+	for(int i = 0; i < len; i++)
+	{
+		sum += a[i];
+	}
+
+	return sum;
+}
+
 int add(int a, int b)
 {
-	return a + b;
+	int arr[2];
+	
+	arr[0] = a;
+	arr[1] = b;
+
+	return add(arr, 2);
 }
 
 int main()
